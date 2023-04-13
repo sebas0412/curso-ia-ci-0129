7418305bc9d0015ffd77651ec5a71d695990bc8d
Gerardo Campos <gerardo.camposbadilla@ucr.ac.cr>
2023-04-12 21:48:49 -0600

    Tome su Push

--- TrabajoEnClase1/main.cpp
+++ TrabajoEnClase1/main.cpp
@@ -3,9 +3,9 @@
 
 int main() {
     int** matrix = new int*[3];
-    matrix[0] = new int[3] {1, 4, 2};
-    matrix[1] = new int[3] {3 ,7 ,5};
-    matrix[2] = new int[3] {6 ,0 ,8};
+    matrix[0] = new int[3] {1, 4, 0};
+    matrix[1] = new int[3] {3 ,7 ,2};
+    matrix[2] = new int[3] {6 ,8 ,5};
 
     auto* a = new AnchoPrimero(matrix);
     a->execute();
